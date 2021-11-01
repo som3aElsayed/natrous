3d58588e2a277e82954033bf32a00b8de943fcbc
Som3a Elsayed Mohamed <som3a9w9w2000@gmail.com>
2021-11-02 01:44:54 +0200

    AFTER COMPRESSTION:

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -66,6 +66,7 @@ app.use(
   })
 );
 
+app.use(compression());
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
---
+++ readme.md
@@ -0,0 +1,3 @@
+# It is a tutrial i was learning
+
+Built using modern techongely Node.js
