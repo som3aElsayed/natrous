b78b0728a371cd5e39c5f0a408865362d0f02591
Som3a Elsayed Mohamed <som3a9w9w2000@gmail.com>
2021-11-02 23:02:56 +0200

    added

--- server.js
+++ server.js
@@ -35,3 +35,10 @@ process.on('unhandledRejection', err => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTEMMMMMM SHITING DOWN GRACFULLY');
+  server.close(() => {
+    console.log('PROCESS IS FINISHED');
+  });
+});
