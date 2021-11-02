0891859fe1c80d020e700ed4a923d5a0272c3696
Som3a Elsayed Mohamed <som3a9w9w2000@gmail.com>
2021-11-03 01:02:58 +0200

    omg fuckk you

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -24,7 +24,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [`${req.protocol}://${req.get('host')}/${tour.imageCover}`],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
@@ -51,8 +51,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 exports.createBookingCheckout = catchAsync(async session => {
   const tour = session.client_reference_id;
-  const { id: user } = await User.findOne({ email: session.customer_email });
-  const price = session.line_items[0].amount / 100;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = amount_total / 100;
   await Booking.create({ tour, user, price });
 });
 exports.webhookCheckOut = async (req, res, next) => {
@@ -67,7 +67,7 @@ exports.webhookCheckOut = async (req, res, next) => {
   } catch (error) {
     return res.status(400).send(`Webhook Error ${err.message}`);
   }
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
   res.status(200).json({ recived: true });
 };
