5e798c0e55542fd062f6d32ae0fef5884487b581
Som3a Elsayed Mohamed <som3a9w9w2000@gmail.com>
2021-11-03 04:15:23 +0200

    srry i will not use programming again

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,7 +54,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = amount_total / 100;
+  const price = session.amount_total / 100;
   console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
